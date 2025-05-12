0d77620263a3f5d6ea657ee5dfd79904b2186947
526LGreen@frhsd.com <526LGreen@frhsd.com>
2025-05-12 10:40:28 -0400

    Second Commit

---
+++ players.js
@@ -0,0 +1,41 @@
+const players = [
+    {
+        firstName:'Artemi',
+        lastName:'Panarin',
+        position:'Center' ,
+        age: 40,
+        photo:'https://picsum.photos/200/300'
+    },
+
+     {
+        firstName : 'Artemi',
+        lastName : 'Panarin',
+        position: 'Center',
+        age: 40,
+        photo: 'https://picsum.photos/200/300'
+    },
+
+    {
+        firstName:'Artemi',
+        lastName :'Panarin',
+        position: 'Center',
+        age: 40,
+        photo: 'https://picsum.photos/200/300'
+    },
+
+     {
+        firstName:'Artemi',
+        lastName : 'Panarin',
+        position: 'Center',
+        age: 40,
+        photo: 'https://picsum.photos/200/300'
+    },
+
+     {
+        firstName:'Artemi',
+        lastName :'Panarin',
+        position: 'Center',
+        age: 40,
+        photo: 'https://picsum.photos/200/300'
+    }
+]
