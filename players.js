019dbf80df3635a5af5a22b66f56b71b5a75aefd
LGreen808 <526LGreen@frhsd.com>
2025-05-20 13:29:55 +0000

    Working pt 2

--- players.js
+++ players.js
@@ -1,41 +1,106 @@
 const players = [
     {
-        firstName:'Artemi',
-        lastName:'Panarin',
-        position:'Center' ,
-        age: 40,
+        firstName:'Nicolas',
+        lastName:'Aube-Kubel',
+        position:'Right Wing' ,
+        age: 29,
         photo:'https://picsum.photos/200/300'
     },
 
      {
-        firstName : 'Artemi',
-        lastName : 'Panarin',
-        position: 'Center',
-        age: 40,
+        firstName : 'Brett',
+        lastName : 'Berard',
+        position: 'Left Wing',
+        age: 22,
         photo: 'https://picsum.photos/200/300'
     },
 
     {
-        firstName:'Artemi',
-        lastName :'Panarin',
+        firstName:'Jonny',
+        lastName :'Brodzinski',
         position: 'Center',
-        age: 40,
+        age: 31,
         photo: 'https://picsum.photos/200/300'
     },
 
      {
-        firstName:'Artemi',
-        lastName : 'Panarin',
+        firstName:'Sam',
+        lastName : 'Carrick',
         position: 'Center',
-        age: 40,
+        age: 33,
         photo: 'https://picsum.photos/200/300'
     },
 
      {
-        firstName:'Artemi',
-        lastName :'Panarin',
-        position: 'Center',
+        firstName:'Will',
+        lastName :'Cuylle',
+        position: 'Left Wing',
         age: 40,
         photo: 'https://picsum.photos/200/300'
     }
+    {
+        firstName:'Adam',
+        lastName :'Edstrom',
+        position: 'Center',
+        age: 25,
+        photo: 'https://picsum.photos/200/300'
+    }
+    {
+        firstName:'Arthur',
+        lastName :'Kaliyev',
+        position: 'Right Wing',
+        age: 23,
+        photo: 'https://picsum.photos/200/300'
+    }
+    {
+        firstName:'Chris',
+        lastName :'Kreider',
+        position: 'Left Wing',
+        age: 34,
+        photo: 'https://picsum.photos/200/300'
+    }
+    {
+        firstName:'Alexis',
+        lastName :'Lafrenière',
+        position: 'Left Wing',
+        age: 40,
+        photo: 'https://picsum.photos/200/300'
+    }
+     {
+        firstName:'J.T.',
+        lastName :'Miller',
+        position: 'Left Wing',
+        age: 32,
+        photo: 'https://picsum.photos/200/300'
+    }
+     {
+        firstName:'Brennan',
+        lastName :'Othmann',
+        position: 'Left Wing',
+        age: 22,
+        photo: 'https://picsum.photos/200/300'
+    }
+     {
+        firstName:'Artemi',
+        lastName :'Panarin',
+        position: 'Left Wing',
+        age: 34,
+        photo: 'https://picsum.photos/200/300'
+    }
+     {
+        firstName:'Juuso',
+        lastName :'Parssinen',
+        position: 'Center',
+        age: 24,
+        photo: 'https://picsum.photos/200/300'
+    }
+
+
+
+
+
+
+
+
+
 ]
