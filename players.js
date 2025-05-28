a0de638ee05b71bc70fa4c705a72ae07595111d9
LGreen808 <526LGreen@frhsd.com>
2025-05-28 15:50:08 +0000

    Added all the pictures for each player on the team roster

--- players.js
+++ players.js
@@ -4,7 +4,7 @@ const players = [
         lastName:'Aube-Kubel',
         position:'Right Wing' ,
         age: 29,
-        photo:'https://picsum.photos/200/300'
+        photo:'imgs/1st.png'
     },
 
      {
@@ -12,7 +12,7 @@ const players = [
         lastName : 'Berard',
         position: 'Left Wing',
         age: 22,
-        photo: 'https://picsum.photos/200/300'
+        photo: 'imgs/2nd.png'
     },
 
     {
@@ -20,7 +20,7 @@ const players = [
         lastName :'Brodzinski',
         position: 'Center',
         age: 31,
-        photo: 'https://picsum.photos/200/300'
+        photo: 'imgs/3rd.png'
     },
 
      {
@@ -28,100 +28,103 @@ const players = [
         lastName : 'Carrick',
         position: 'Center',
         age: 33,
-        photo: 'https://picsum.photos/200/300'
+        photo: 'imgs/4th.png'
     },
 
      {
         firstName:'Will',
         lastName :'Cuylle',
         position: 'Left Wing',
-        age: 40,
-        photo: 'https://picsum.photos/200/300'
-    }
+        age: 23,
+        photo: 'imgs/5th.png'
+    },
     {
         firstName:'Adam',
         lastName :'Edstrom',
         position: 'Center',
-        age: 25,
-        photo: 'https://picsum.photos/200/300'
-    }
+        age: 24,
+        photo: 'imgs/6th.png'
+    },
     {
         firstName:'Arthur',
         lastName :'Kaliyev',
         position: 'Right Wing',
         age: 23,
-        photo: 'https://picsum.photos/200/300'
-    }
+        photo: 'imgs/7th.png'
+    },
     {
         firstName:'Chris',
         lastName :'Kreider',
         position: 'Left Wing',
         age: 34,
-        photo: 'https://picsum.photos/200/300'
-    }
+        photo: 'imgs/8th.png'
+    },
     {
         firstName:'Alexis',
         lastName :'Lafrenière',
         position: 'Left Wing',
-        age: 40,
-        photo: 'https://picsum.photos/200/300'
-    }
+        age: 23,
+        photo: 'imgs/9th.png'
+    },
      {
         firstName:'J.T.',
         lastName :'Miller',
         position: 'Left Wing',
         age: 32,
-        photo: 'https://picsum.photos/200/300'
-    }
+        photo: 'imgs/10th.png'
+    },
      {
         firstName:'Brennan',
         lastName :'Othmann',
         position: 'Left Wing',
         age: 22,
-        photo: 'https://picsum.photos/200/300'
-    }
+        photo: 'imgs/11th.png'
+    },
      {
         firstName:'Artemi',
         lastName :'Panarin',
         position: 'Left Wing',
-        age: 34,
-        photo: 'https://picsum.photos/200/300'
-    }
+        age: 33,
+        photo: 'imgs/12th.png'
+    },
      {
         firstName:'Juuso',
         lastName :'Parssinen',
         position: 'Center',
         age: 24,
-        photo: 'https://picsum.photos/200/300'
-    }
+        photo: 'imgs/13th.png'
+    },
      {
         firstName:'Gabe',
         lastName :'Perreault',
         position: 'Right Wing',
         age: 20,
-        photo: 'https://picsum.photos/200/300'
-    }
+        photo: 'imgs/14th.png'
+    },
      {
         firstName:'Matt',
         lastName :'Rempe',
         position: 'Center',
         age: 22,
-        photo: 'https://picsum.photos/200/300'
-    }
+        photo: 'imgs/15th.png'
+    },
      {
         firstName:'Vincent',
         lastName :'Trocheck',
         position: 'Center',
         age: 31,
-        photo: 'https://picsum.photos/200/300'
-    }
+        photo: 'imgs/16th.png'
+    },
      {
         firstName:'Mika',
         lastName :'Zibanejad',
         position: 'Center',
         age: 32,
-        photo: 'https://picsum.photos/200/300'
-    }
+        photo: 'imgs/17th.png'
+    },
+
+
+
 
 
 
@@ -129,70 +132,70 @@ const players = [
         firstName:'Will',
         lastName :'Borgen',
         position: 'Defense',
-        age: 29,
-        photo: 'https://picsum.photos/200/300'
-    }
+        age: 28,
+        photo: 'imgs/18th.png'
+    },
      {
         firstName:'Calvin',
         lastName :'Haan',
         position: 'Defense',
         age: 34,
-        photo: 'https://picsum.photos/200/300'
-    }
+        photo: 'imgs/19th.png'
+    },
      {
         firstName:'Adam',
         lastName :'Fox',
         position: 'Defense',
         age: 27,
-        photo: 'https://picsum.photos/200/300'
-    }
+        photo: 'imgs/20th.png'
+    },
      {
         firstName:'Zac',
         lastName :'Jones',
         position: 'Defense',
         age: 24,
-        photo: 'https://picsum.photos/200/300'
-    }
+        photo: 'imgs/21st.png'
+    },
      {
         firstName:'K`Andre',
         lastName :'Miller',
         position: 'Defense',
         age: 25,
-        photo: 'https://picsum.photos/200/300'
-    }
+        photo: 'imgs/22nd.png'
+    },
      {
         firstName:'Braden',
         lastName :'Schneider',
         position: 'Defense',
         age: 23,
-        photo: 'https://picsum.photos/200/300'
-    }
+        photo: 'imgs/23rd.png'
+    },
      {
         firstName:'Carson',
         lastName :'Soucy',
         position: 'Defense',
         age: 30,
-        photo: 'https://picsum.photos/200/300'
-    }
+        photo: 'imgs/24th.png'
+    },
  {
         firstName:'Urho',
         lastName :'Vaakanainen',
         position: 'Defense',
         age: 26,
-        photo: 'https://picsum.photos/200/300'
-    }
+        photo: 'imgs/25th.png'
+    },
      {
         firstName:'Jonathan',
         lastName :'Quick',
         position: 'Goalie',
         age: 39,
-        photo: 'https://picsum.photos/200/300'
-    }
+        photo: 'imgs/26th.png'
+    },
      {
         firstName:'Igor',
         lastName :'Shesterkin',
         position: 'Goalie',
         age: 29,
-        photo: 'https://picsum.photos/200/300'
+        photo: 'imgs/27th.png'
     }   
 ]
